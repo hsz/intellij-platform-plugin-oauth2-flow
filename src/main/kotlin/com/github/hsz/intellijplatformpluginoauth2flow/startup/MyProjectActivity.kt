5a4ab0d0000bd0e395d12cf89cc046a2d70ff03b
GitHub Action <action@github.com>
2026-01-16 19:18:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/hsz/intellijplatformpluginoauth2flow/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hsz/intellijplatformpluginoauth2flow/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hsz.intellijplatformpluginoauth2flow.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
